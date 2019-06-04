930951c494df51a1e6d879539905bf22817700f6
Javier Brito Pacheco <jbritopc@gmail.com>
2019-06-04 18:10:22 -0500

    updates

--- src/main/java/mx/freshmanasoft/phs/package-info.java
+++
@@ -1,1 +0,0 @@
-package mx.freshmanasoft.phs;
--- src/main/java/src/main/java/mx/freshmanasoft/phs/Application.java
+++ src/main/java/src/main/java/mx/freshmanasoft/phs/Application.java
@@ -1,4 +1,4 @@
-package mx.freshmanasoft.phs;
+package src.main.java.mx.freshmanasoft.phs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
