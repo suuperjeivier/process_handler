80662652f103246286f3f502c6487645dd46879b
LockyGT <isnavest@live.com.mx>
2019-06-29 19:47:59 -0500

    [CORRECCIÓN] En las horas del log de usuarios.

--- src/main/java/mx/freshmanasoft/phs/AspectLoggin.java
+++ src/main/java/mx/freshmanasoft/phs/AspectLoggin.java
@@ -1,6 +1,7 @@
 package mx.freshmanasoft.phs;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
@@ -26,13 +27,16 @@ public class AspectLoggin {
 	private Loggin loggin = null;
 	private String logginUser;
 	private String logoutUser;
+	private static final String DATE_FORMATTER= "yyyy-MM-dd HH:mm:ss";
+	DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+	
 	@After("execution(* mx.freshmanasoft.phs.controller.BankController.*(..))")
 	public void logBank(JoinPoint point) {
-		
+		LocalDateTime localDateTime = LocalDateTime.now();
 		loggin = new Loggin();
 		loggin.setMethod(point.getSignature().getName());
 		loggin.setEntity("Bank");
-		loggin.setDate(LocalDateTime.now().toString());
+		loggin.setDate(localDateTime.format(FORMATTER));
 		String params = "";
 		for(Object j : point.getArgs()) {
 			params += j.toString(); 
@@ -44,11 +48,11 @@ public class AspectLoggin {
 	
 	@After("execution(* mx.freshmanasoft.phs.controller.BankAccountController.*(..))")
 	public void logBankAccount(JoinPoint point) {
-		
+		LocalDateTime localDateTime = LocalDateTime.now();
 		loggin = new Loggin();
 		loggin.setMethod(point.getSignature().getName());
 		loggin.setEntity("Bank Account");
-		loggin.setDate(LocalDateTime.now().toString());
+		loggin.setDate(localDateTime.format(FORMATTER));
 		String params = "";
 		for(Object j : point.getArgs()) {
 			params += j.toString(); 
@@ -60,11 +64,11 @@ public class AspectLoggin {
 	
 	@After("execution(* mx.freshmanasoft.phs.controller.BankActionController.*(..))")
 	public void logBankAction(JoinPoint point) {
-		
+		LocalDateTime localDateTime = LocalDateTime.now();
 		loggin = new Loggin();
 		loggin.setMethod(point.getSignature().getName());
 		loggin.setEntity("Bank Action");
-		loggin.setDate(LocalDateTime.now().toString()); 
+		loggin.setDate(localDateTime.format(FORMATTER)); 
 		loggin.setUser(logginUser);
 		String params = "";
 		for(Object j : point.getArgs()) {
@@ -76,11 +80,11 @@ public class AspectLoggin {
 	
 	@After("execution(* mx.freshmanasoft.phs.controller.CompanyController.*(..))")
 	public void logCompany(JoinPoint point) {
-		
+		LocalDateTime localDateTime = LocalDateTime.now();
 		loggin = new Loggin();
 		loggin.setMethod(point.getSignature().getName());
 		loggin.setEntity("Company");
-		loggin.setDate(LocalDateTime.now().toString()); 
+		loggin.setDate(localDateTime.format(FORMATTER)); 
 		loggin.setUser(logginUser);
 		String params = "";
 		for(Object j : point.getArgs()) {
@@ -92,11 +96,11 @@ public class AspectLoggin {
 	
 	@After("execution(* mx.freshmanasoft.phs.controller.MainController.*(..))")
 	public void logMain(JoinPoint point) {
-		
+		LocalDateTime localDateTime = LocalDateTime.now();
 		loggin = new Loggin();
 		loggin.setMethod(point.getSignature().getName());
 		loggin.setEntity("Main");
-		loggin.setDate(LocalDateTime.now().toString()); 
+		loggin.setDate(localDateTime.format(FORMATTER)); 
 		loggin.setUser(logginUser);
 		String params = "";
 		for(Object j : point.getArgs()) {
@@ -109,25 +113,27 @@ public class AspectLoggin {
 	 @AfterReturning(pointcut="execution(* org.springframework.security.authentication.AuthenticationManager.authenticate(..))"
 	            ,returning="result")
 	    public void after(JoinPoint point,Object result) throws Throwable {
+		 LocalDateTime localDateTime = LocalDateTime.now();
 		 	System.out.println(">>> LOGGIN USER: " + ((Authentication) result).getName());
 	        logginUser = ((Authentication) result).getName();
 			loggin = new Loggin();
 			loggin.setMethod(point.getSignature().getName());
 			loggin.setEntity("Main");
-			loggin.setDate(LocalDateTime.now().toString()); 
+			loggin.setDate(localDateTime.format(FORMATTER)); 
 			loggin.setUser(logginUser);
 			loggin.setData("security");
 			repository.save(loggin);
 	    }
-	 @Before("execution(* org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler.logout(..))")
-	 public void beforeLogout(JoinPoint joinPoint) throws Throwable {      
+	 @Before("execution(* org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler.*(..))")
+	 public void beforeLogout(JoinPoint joinPoint) throws Throwable {   
+		 LocalDateTime localDateTime = LocalDateTime.now();
 		 logoutUser = SecurityContextHolder.getContext().getAuthentication().getName();
 	     System.out.println(
 	                 ">>> Aspect : User " +  logoutUser + " successfully logged out.");
 	     loggin = new Loggin();
 			loggin.setMethod(joinPoint.getSignature().getName());
 			loggin.setEntity("Main");
-			loggin.setDate(LocalDateTime.now().toString()); 
+			loggin.setDate(localDateTime.format(FORMATTER)); 
 			loggin.setUser(logoutUser);
 			loggin.setData("security");
 			repository.save(loggin);
